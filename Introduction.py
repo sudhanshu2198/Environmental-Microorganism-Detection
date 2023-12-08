329495d92e5debc4c57d10f7bd5114528e598bc9
Sudhanshu Rastogi <102512385+sudhanshu2198@users.noreply.github.com>
2023-12-08 13:41:08 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -10,13 +10,20 @@ homepage_img_dir=os.path.join(root_dir,"images","home_page")
 
 st.title("Microorganism Detection in Water")
 st.subheader("Project Description")
-st.write("Environmental microorganisms (EMs) are ubiquitous around us and have an important impact on the survival\
-         and development of human society. The detection of environmental microorganism indicators is essential for \
-         for assessing the degree of pollution. Faster-RCNN, YOLOv3, SSD and RetinaNet object detection algorithm\
-         are utilized are detecting microorganism in water...........")
+st.write("In the aqueous tapestry that sustains life, the role of environmental microorganisms (EMs) in water takes \
+          center stage, especially when it comes to the paramount concern of public health. Every drop of water tells a \
+          story, and within it, microscopic actors play a crucial part. Detecting these environmental microorganisms is \
+          not just a scientific endeavor but a vital step in safeguarding communities against waterborne threats. From \
+          identifying potential pathogens that could jeopardize public health to ensuring the purity of our drinking water sources, \
+          the microscopic world within water holds the answers. By delving into the unseen realms, we equip ourselves with the \
+          knowledge needed to protect the most fundamental resource we all share—clean and safe water for the well-being of everyone.\
+          The detection of environmental microorganism indicators is essential for assessing the degree of pollution. Faster-RCNN, \
+          YOLOv3, SSD and RetinaNet object detection algorithm are utilized are detecting microorganism in water ")
 
 st.subheader("Dataset")
-st.write("The Environmental Microorganism Image Dataset Seventh Version is a microscopic image data set, including\
-             the original Environmental Microorganism images (EMs) and the corresponding object labeling files in xml\
-             format file. The EMDS-7 data set consists of 41 types of EMs, which has a total of 2365 images and 13216 \
-             labeled objects...........")
+st.write("The overall dataset is made up of the following two datasets:")
+st.markdown("""
+            - Environmental Microorganism Image Dataset Seventh Version is a microscopic image data set containing 41 types of EMs, with 2,65 \
+            images and 13,216 labeled objects in XML Format.
+            - Environmental Microorganism Image Dataset Sixth Version is a microscopic image data set containing 21 types of EMs. Each type of EM \
+            contains 40 original and 40 GT images, in total 1680 EM ages. Box annotations were generated using the Roboflow tool, generating XML and YoloV8 files.""")
